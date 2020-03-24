e36599adf31b455abcfd0f75311930cbe6c107d8
a1824jj <a1824jj@aiit.ac.jp>
2020-03-24 11:25:59 +0900

    first commit

---
+++ app/src/main/java/a1824jj/jp/ac/aiit/navisampel1/MainActivity.java
@@ -0,0 +1,19 @@
+package a1824jj.jp.ac.aiit.navisampel1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import a1824jj.jp.ac.aiit.navisampel1.ui.main.MainFragment;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_activity);
+
+
+
+    }
+}
---
+++ app/src/main/java/a1824jj/jp/ac/aiit/navisampel1/ui/main/MainViewModel.java
@@ -0,0 +1,7 @@
+package a1824jj.jp.ac.aiit.navisampel1.ui.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class MainViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
